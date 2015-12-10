d78561736c8d10a0c0cdb7de97b6ab34fdb1160f
McJty <mcjty1@gmail.com>
2015-12-10 14:18:11 +0100

    Stuff

--- src/main/java/romelo333/notenoughwands/proxy/ClientProxy.java
+++ src/main/java/romelo333/notenoughwands/proxy/ClientProxy.java
@@ -1,18 +1,19 @@
 package romelo333.notenoughwands.proxy;
 
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.entity.EntityPlayerSP;
+import net.minecraft.item.ItemStack;
 import net.minecraftforge.client.event.RenderWorldLastEvent;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.fml.common.FMLCommonHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import romelo333.notenoughwands.Items.GenericWand;
 import romelo333.notenoughwands.KeyBindings;
 import romelo333.notenoughwands.KeyInputHandler;
 import romelo333.notenoughwands.ModItems;
 import romelo333.notenoughwands.ModRenderers;
-
-//import net.minecraft.client.entity.EntityClientPlayerMP;
 
 public class ClientProxy extends CommonProxy {
 
@@ -27,22 +28,22 @@ public class ClientProxy extends CommonProxy {
         ModRenderers.init();
         ModItems.initModels();
         MinecraftForge.EVENT_BUS.register(this);
-        FMLCommonHandler.instance().bus().register(new KeyInputHandler());
+        MinecraftForge.EVENT_BUS.register(new KeyInputHandler());
         KeyBindings.init();
     }
 
     @SubscribeEvent
     public void renderWorldLastEvent(RenderWorldLastEvent evt) {
-//        Minecraft mc = Minecraft.getMinecraft();
-//        EntityClientPlayerMP p = mc.thePlayer;
-//        ItemStack heldItem = p.getHeldItem();
-//        if (heldItem == null) {
-//            return;
-//        }
-//        if (heldItem.getItem() instanceof GenericWand) {
-//            GenericWand genericWand = (GenericWand) heldItem.getItem();
-//            genericWand.renderOverlay(evt, p, heldItem);
-//        }
+        Minecraft mc = Minecraft.getMinecraft();
+        EntityPlayerSP p = mc.thePlayer;
+        ItemStack heldItem = p.getHeldItem();
+        if (heldItem == null) {
+            return;
+        }
+        if (heldItem.getItem() instanceof GenericWand) {
+            GenericWand genericWand = (GenericWand) heldItem.getItem();
+            genericWand.renderOverlay(evt, p, heldItem);
+        }
     }
 
     @Override
