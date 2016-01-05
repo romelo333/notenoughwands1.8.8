ed5bae00055bc5d47ae60cb1c4ad3000eb364416
McJty <mcjty1@gmail.com>
2016-01-05 10:01:16 +0100

    Updated NEW to new forge and jei/waila

--- src/main/java/romelo333/notenoughwands/NotEnoughWands.java
+++ src/main/java/romelo333/notenoughwands/NotEnoughWands.java
@@ -20,7 +20,7 @@ import java.io.File;
         version = NotEnoughWands.VERSION)
 public class NotEnoughWands {
     public static final String MODID = "notenoughwands";
-    public static final String VERSION = "1.2.3";
+    public static final String VERSION = "1.2.4";
     public static final String MIN_FORGE_VER = "11.15.0.1600";
 
     @SidedProxy(clientSide="romelo333.notenoughwands.proxy.ClientProxy", serverSide="romelo333.notenoughwands.proxy.ServerProxy")
