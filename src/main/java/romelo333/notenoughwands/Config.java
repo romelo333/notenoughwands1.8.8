2e07ce12bafd6f6ccc1e48a0732a2eecbd93b894
McJty <mcjty1@gmail.com>
2016-05-02 11:36:31 +0200

    Changed 'wands' config category to 'wandsettings' to ensure the config gets regenerated properly

--- src/main/java/romelo333/notenoughwands/Config.java
+++ src/main/java/romelo333/notenoughwands/Config.java
@@ -5,7 +5,7 @@ import net.minecraftforge.common.config.Configuration;
 import romelo333.notenoughwands.Items.GenericWand;
 
 public class Config {
-    public static String CATEGORY_WANDS = "wands";
+    public static String CATEGORY_WANDS = "wandsettings";
     public static String CATEGORY_MOVINGBLACKLIST = "movingblacklist";
 
     public static void init(Configuration cfg) {
