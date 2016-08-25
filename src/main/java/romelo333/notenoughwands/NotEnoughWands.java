045fe6e19c0620ee8ac02be9aa4e52561a22f8a7
McJty <mcjty1@gmail.com>
2016-08-25 19:47:04 +0200

    1.3.6

--- src/main/java/romelo333/notenoughwands/NotEnoughWands.java
+++ src/main/java/romelo333/notenoughwands/NotEnoughWands.java
@@ -20,7 +20,7 @@ import java.io.File;
         version = NotEnoughWands.VERSION)
 public class NotEnoughWands {
     public static final String MODID = "notenoughwands";
-    public static final String VERSION = "1.3.5";
+    public static final String VERSION = "1.3.6";
     public static final String MIN_FORGE_VER = "12.16.1.1896";
 
     @SidedProxy(clientSide="romelo333.notenoughwands.proxy.ClientProxy", serverSide="romelo333.notenoughwands.proxy.ServerProxy")
