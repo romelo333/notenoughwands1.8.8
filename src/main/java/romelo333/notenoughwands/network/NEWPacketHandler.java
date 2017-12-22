2932584c80e37b1d33c0514ba0bd00a2426c3f6a
McJty <mcjty1@gmail.com>
2017-12-22 12:58:39 +0100

    Networking stuff

--- src/main/java/romelo333/notenoughwands/network/NEWPacketHandler.java
+++ src/main/java/romelo333/notenoughwands/network/NEWPacketHandler.java
@@ -1,31 +1,26 @@
 package romelo333.notenoughwands.network;
 
 
-import net.minecraftforge.fml.common.network.NetworkRegistry;
+import mcjty.lib.network.PacketHandler;
 import net.minecraftforge.fml.common.network.simpleimpl.SimpleNetworkWrapper;
 import net.minecraftforge.fml.relauncher.Side;
 
 public class NEWPacketHandler {
     public static SimpleNetworkWrapper INSTANCE;
-    private static int ID = 0;
-
-    public static int nextID() {
-        return ID++;
-    }
 
     public static void registerMessages(SimpleNetworkWrapper network) {
         INSTANCE = network;
 
         // Server side
-        INSTANCE.registerMessage(PacketToggleMode.Handler.class, PacketToggleMode.class, nextID(), Side.SERVER);
-        INSTANCE.registerMessage(PacketToggleSubMode.Handler.class, PacketToggleSubMode.class, nextID(), Side.SERVER);
-        INSTANCE.registerMessage(PacketGetProtectedBlocks.Handler.class, PacketGetProtectedBlocks.class, nextID(), Side.SERVER);
-        INSTANCE.registerMessage(PacketGetProtectedBlockCount.Handler.class, PacketGetProtectedBlockCount.class, nextID(), Side.SERVER);
-        INSTANCE.registerMessage(PacketGetProtectedBlocksAroundPlayer.Handler.class, PacketGetProtectedBlocksAroundPlayer.class, nextID(), Side.SERVER);
+        INSTANCE.registerMessage(PacketToggleMode.Handler.class, PacketToggleMode.class, PacketHandler.nextID(), Side.SERVER);
+        INSTANCE.registerMessage(PacketToggleSubMode.Handler.class, PacketToggleSubMode.class, PacketHandler.nextID(), Side.SERVER);
+        INSTANCE.registerMessage(PacketGetProtectedBlocks.Handler.class, PacketGetProtectedBlocks.class, PacketHandler.nextID(), Side.SERVER);
+        INSTANCE.registerMessage(PacketGetProtectedBlockCount.Handler.class, PacketGetProtectedBlockCount.class, PacketHandler.nextID(), Side.SERVER);
+        INSTANCE.registerMessage(PacketGetProtectedBlocksAroundPlayer.Handler.class, PacketGetProtectedBlocksAroundPlayer.class, PacketHandler.nextID(), Side.SERVER);
 
         // Client side
-        INSTANCE.registerMessage(PacketReturnProtectedBlocks.Handler.class, PacketReturnProtectedBlocks.class, nextID(), Side.CLIENT);
-        INSTANCE.registerMessage(PacketReturnProtectedBlockCount.Handler.class, PacketReturnProtectedBlockCount.class, nextID(), Side.CLIENT);
-        INSTANCE.registerMessage(PacketReturnProtectedBlocksAroundPlayer.Handler.class, PacketReturnProtectedBlocksAroundPlayer.class, nextID(), Side.CLIENT);
+        INSTANCE.registerMessage(PacketReturnProtectedBlocks.Handler.class, PacketReturnProtectedBlocks.class, PacketHandler.nextID(), Side.CLIENT);
+        INSTANCE.registerMessage(PacketReturnProtectedBlockCount.Handler.class, PacketReturnProtectedBlockCount.class, PacketHandler.nextID(), Side.CLIENT);
+        INSTANCE.registerMessage(PacketReturnProtectedBlocksAroundPlayer.Handler.class, PacketReturnProtectedBlocksAroundPlayer.class, PacketHandler.nextID(), Side.CLIENT);
     }
 }
