4b78a24ace3a4c042c77095d6d1aca39d4624cca
romelo333 <romelo333@gmail.com>
2018-05-28 20:44:39 +0200

    More project updates

--- src/main/java/romelo333/notenoughwands/NotEnoughWands.java
+++ src/main/java/romelo333/notenoughwands/NotEnoughWands.java
@@ -24,10 +24,10 @@ import java.io.File;
         version = NotEnoughWands.VERSION)
 public class NotEnoughWands implements ModBase {
     public static final String MODID = "notenoughwands";
-    public static final String VERSION = "1.6.1";
+    public static final String VERSION = "1.7.0alpha";
     public static final String MIN_FORGE11_VER = "13.19.0.2176";
     public static final String MIN_COFH_VER = "2.0.0";
-    public static final String MIN_MCJTYLIB_VER = "2.6.0";
+    public static final String MIN_MCJTYLIB_VER = "3.0.0";
 
     @SidedProxy(clientSide="romelo333.notenoughwands.proxy.ClientProxy", serverSide="romelo333.notenoughwands.proxy.ServerProxy")
     public static CommonProxy proxy;
