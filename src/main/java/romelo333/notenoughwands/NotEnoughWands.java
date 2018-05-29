b5a366af9e25f46a5af00426c01f7b1137072715
McJty <mcjty1@gmail.com>
2018-05-29 09:35:49 +0200

    Fixed versions in all mods

--- src/main/java/romelo333/notenoughwands/NotEnoughWands.java
+++ src/main/java/romelo333/notenoughwands/NotEnoughWands.java
@@ -24,10 +24,10 @@ import java.io.File;
         version = NotEnoughWands.VERSION)
 public class NotEnoughWands implements ModBase {
     public static final String MODID = "notenoughwands";
-    public static final String VERSION = "1.7.0alpha";
+    public static final String VERSION = "1.7.0-alpha";
     public static final String MIN_FORGE11_VER = "13.19.0.2176";
     public static final String MIN_COFH_VER = "2.0.0";
-    public static final String MIN_MCJTYLIB_VER = "3.0.0";
+    public static final String MIN_MCJTYLIB_VER = "3.0.0-alpha";
 
     @SidedProxy(clientSide="romelo333.notenoughwands.proxy.ClientProxy", serverSide="romelo333.notenoughwands.proxy.ServerProxy")
     public static CommonProxy proxy;
