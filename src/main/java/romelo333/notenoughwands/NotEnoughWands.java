9f7113548bb176b2ff987770507a81e9dc949e0a
McJty <mcjty1@gmail.com>
2019-04-30 18:50:43 +0200

    1.8.1

--- src/main/java/romelo333/notenoughwands/NotEnoughWands.java
+++ src/main/java/romelo333/notenoughwands/NotEnoughWands.java
@@ -19,7 +19,7 @@ import romelo333.notenoughwands.setup.ModSetup;
         version = NotEnoughWands.VERSION)
 public class NotEnoughWands implements ModBase {
     public static final String MODID = "notenoughwands";
-    public static final String VERSION = "1.8.0";
+    public static final String VERSION = "1.8.1";
     public static final String MIN_FORGE11_VER = "13.19.0.2176";
     public static final String MIN_COFH_VER = "2.0.0";
     public static final String MIN_MCJTYLIB_VER = "3.5.0";
