608558b102929fb700bf4e4a1f02b98934bda667
McJty <mcjty1@gmail.com>
2019-05-23 15:36:12 +0200

    1.7.4

--- src/main/java/romelo333/notenoughwands/NotEnoughWands.java
+++ src/main/java/romelo333/notenoughwands/NotEnoughWands.java
@@ -10,7 +10,7 @@ import java.io.File;
 
 public class NotEnoughWands implements ModInitializer {
     public static final String MODID = "notenoughwands";
-    public static final String VERSION = "1.7.3";
+    public static final String VERSION = "1.7.4";
     public static final String MIN_FORGE11_VER = "13.19.0.2176";
     public static final String MIN_COFH_VER = "2.0.0";
     public static final String MIN_MCJTYLIB_VER = "3.0.0";
