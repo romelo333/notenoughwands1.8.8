436c39549b9de6c64a9b89b879bfd87b4a10cbb9
McJty <mcjty1@gmail.com>
2023-06-19 21:18:33 +0200

    The creative tabs work

--- src/main/java/romelo333/notenoughwands/setup/Registration.java
+++ src/main/java/romelo333/notenoughwands/setup/Registration.java
@@ -1,13 +1,20 @@
 package romelo333.notenoughwands.setup;
 
+import net.minecraft.core.registries.Registries;
+import net.minecraft.network.chat.Component;
 import net.minecraft.sounds.SoundEvent;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
 import romelo333.notenoughwands.NotEnoughWands;
+import romelo333.notenoughwands.modules.wands.WandsModule;
 
 import static romelo333.notenoughwands.NotEnoughWands.MODID;
 
@@ -17,15 +24,26 @@ public class Registration {
     public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, MODID);
     public static final DeferredRegister<BlockEntityType<?>> TILES = DeferredRegister.create(ForgeRegistries.BLOCK_ENTITY_TYPES, MODID);
     public static final DeferredRegister<SoundEvent> SOUNDS = DeferredRegister.create(ForgeRegistries.SOUND_EVENTS, MODID);
+    public static final DeferredRegister<CreativeModeTab> TABS = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, MODID);
 
     public static void register() {
         BLOCKS.register(FMLJavaModLoadingContext.get().getModEventBus());
         ITEMS.register(FMLJavaModLoadingContext.get().getModEventBus());
         TILES.register(FMLJavaModLoadingContext.get().getModEventBus());
         SOUNDS.register(FMLJavaModLoadingContext.get().getModEventBus());
+        TABS.register(FMLJavaModLoadingContext.get().getModEventBus());
     }
 
     public static Item.Properties createStandardProperties() {
         return NotEnoughWands.setup.defaultProperties();
     }
+
+    public static RegistryObject<CreativeModeTab> TAB = TABS.register("notenoughwands", () -> CreativeModeTab.builder()
+            .title(Component.translatable("itemGroup." + MODID))
+            .icon(() -> new ItemStack(WandsModule.WAND_CORE.get()))
+            .withTabsBefore(CreativeModeTabs.SPAWN_EGGS)
+            .displayItems((featureFlags, output) -> {
+                NotEnoughWands.setup.populateTab(output);
+            })
+            .build());
 }
